abe0323d439f90e5949cabd01eaabd6b8b75e815
David Sevillano <dvd.sevillano@gmail.com>
2025-01-27 11:55:03 +0100

    Estructura de carpetas + Dataseed + Dependencia swagger

---
+++ conecta/src/main/java/com/salesianos/conecta/util/DataSeed.java
@@ -0,0 +1,16 @@
+package com.salesianos.conecta.util;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataSeed {
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+}
