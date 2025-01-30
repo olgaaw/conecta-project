2a14c4c569bbf689eae643c4f922123be92ecb01
Burixer85 <dvd.sevillano@gmail.com>
2025-01-30 20:03:31 +0100

    HU-21 terminado (Elimina una empresa por id)

--- conecta/src/main/java/com/salesianos/conecta/controller/EmpresaController.java
+++ conecta/src/main/java/com/salesianos/conecta/controller/EmpresaController.java
@@ -4,10 +4,7 @@ import com.salesianos.conecta.dto.GetEmpresaDto;
 import com.salesianos.conecta.service.EmpresaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,12 @@ public class EmpresaController {
     @GetMapping("{id}")
     public GetEmpresaDto getById(@PathVariable Long id){
         return GetEmpresaDto.of(empresaService.findById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        empresaService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
