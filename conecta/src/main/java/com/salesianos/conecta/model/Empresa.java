bec172f8c72fd92e828d68b5712a71feaff3d176
DavidSevillano <157105926+DavidSevillano@users.noreply.github.com>
2025-02-03 12:52:11 +0100

    Update Empresa.java

--- conecta/src/main/java/com/salesianos/conecta/model/Empresa.java
+++ conecta/src/main/java/com/salesianos/conecta/model/Empresa.java
@@ -17,7 +17,7 @@ import java.util.*;
 public class Empresa {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String cif;
