17e84191314b4813e21b8176e90a86e56d3da9f1
David Sevillano <dvd.sevillano@gmail.com>
2025-02-03 09:00:35 +0100

    HU-20 terminada

--- conecta/src/main/java/com/salesianos/conecta/dto/GetFamiliasProfesionalesDto.java
+++ conecta/src/main/java/com/salesianos/conecta/dto/GetFamiliasProfesionalesDto.java
@@ -7,15 +7,10 @@ public record GetFamiliasProfesionalesDto(
     String nombre
             ){
 
-
     public static GetFamiliasProfesionalesDto of(FamiliaProfesional familiaProfesional){
         return new GetFamiliasProfesionalesDto(
                 familiaProfesional.getNombre());
 
-    }
-
-    public FamiliaProfesional toFamiliaProfesional() {
-        return new FamiliaProfesional(this.nombre); // Asegúrate de que el constructor de FamiliaProfesional acepte un String
     }
 
 }
